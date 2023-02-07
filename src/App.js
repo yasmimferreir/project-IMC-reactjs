7b54df970fc975986ff7fa94b60024736aac7496
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-02-07 10:17:45 -0400

    Projeto calculadora de  IMC com Reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div className="app-container">
+        <h1>Calculadora IMC</h1>
+        <span>Vamos calcular seu IMC</span>
+
+        <div className="container-input">
+          <input type="text"
+            placeholder="Digite seu Peso" />
+          <input type="text"
+            placeholder="Digite sua Altura" />
+          <button>Calcular</button>
+        </div>
+
+        <h2>Seu IMC foi "number", você está abaixo do peso</h2>
+
+      </div>
+
+
+    </div >
   );
 }
 
