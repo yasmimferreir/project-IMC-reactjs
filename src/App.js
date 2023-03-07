1bb970561dd3a07a34d51b12dc0bdffb53e2fb13
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-03-07 15:30:01 -0400

    atualização/CalculadoraImc

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
 import React from "react";
+import { useState } from "react";
 import "./app.css";
+import imcImage from "./assets/imc-iphone.png";
 
 function App() {
+  const [peso, setPeso] = useState("");
+  const [altura, setAltura] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  function calcularImc() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18.6) {
+      setMensagem("Voce está abaixo do peso! Seu IMC é : " + imc.toFixed(2));
+    } else if (imc >= 18.6 && imc < 24.9) {
+      setMensagem("Peso ideal! Seu IMC é : " + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      setMensagem("Voce está um pouco acima do peso! Seu IMC é : " + imc.toFixed(2));
+    } else if (imc >= 34.9) {
+      setMensagem("Voce está  acima do peso! Seu IMC é : " + imc.toFixed(2));
+    }
+  }
+
   return (
     <div className="app">
-      <div className="app-container">
-        <h1>Calculadora IMC</h1>
-        <span>Vamos calcular seu IMC</span>
-
-        <div className="container-input">
-          <input type="text"
-            placeholder="Digite seu Peso" />
-          <input type="text"
-            placeholder="Digite sua Altura" />
-          <button>Calcular</button>
-        </div>
-
-        <h2>Seu IMC foi "number", você está abaixo do peso</h2>
-
+      <div className="container-image">
+       <img src={imcImage}  alt="img-Imc"/>
       </div>
 
 
-    </div >
+      <div className="app-container">
+        <h1>
+          Calculadora <span className="title-span"> imc</span>
+        </h1>
+        <span className="paragrafh">
+          Preencha os dados do seu peso e altura 
+        </span>
+
+        <div className="container-input">
+          <p>Peso</p>
+          <input
+            type="text"
+            required="Preencha os dados"
+            placeholder="digite seu peso"
+            value={peso}
+            onChange={(e) => setPeso(e.target.value)}
+          />
+          <p>Altura</p>
+          <input
+            type="text"
+            
+            placeholder="digite sua altura"
+            value={altura}
+            onChange={(e) => setAltura(e.target.value)}
+            required="Preencha os dados"
+          />
+
+          <button onClick={calcularImc}>Calcular</button>
+        </div>
+
+        <h2 className="mensagem-imc">{mensagem}</h2>
+      </div>
+    </div>
   );
 }
 
